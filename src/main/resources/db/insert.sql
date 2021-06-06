10b36f729cf9122d9f12f2e0a0e22a60787b522e
ogneyar79 <maksimus.s@mail.ru>
2021-06-06 23:18:45 +0300

    InsertLiquidBase

---
+++ src/main/resources/db/insert.sql
@@ -0,0 +1,4 @@
+insert into postss (name) values ('О чем этот форум?');
+insert into postss (name) values ('Правила форума.');
+insert into authoritiess (authority) values ('ROLE_USER');
+insert into authoritiess (authority) values ('ROLE_ADMIN');
---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,23 @@
+create table posts
+(
+    id          serial primary key,
+    name        varchar(2000),
+    description text,
+    created     timestamp without time zone not null default now()
+);
+
+CREATE TABLE authorities
+(
+    id        serial primary key,
+    authority VARCHAR(50) NOT NULL unique
+);
+
+CREATE TABLE users
+(
+    id           serial primary key,
+    username     VARCHAR(50)  NOT NULL unique,
+    email        text         NOT NULL,
+    password     VARCHAR(100) NOT NULL,
+    enabled      boolean default true,
+    authority_id int          not null references authorities (id)
+);
